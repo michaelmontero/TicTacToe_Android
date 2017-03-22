0f6d43f1ba470e724f577665de927705d6ab6c38
Michael Montero <monterom334@gmail.com>
2017-03-22 08:08:39 +0100

    Enable play agains the machine

--- app/src/main/java/com/example/elleu/tictactoe_android/Game.java
+++ app/src/main/java/com/example/elleu/tictactoe_android/Game.java
@@ -1,19 +1,29 @@
 package com.example.elleu.tictactoe_android;
 
+import java.util.Random;
+
 /**
  * Created by Michael Montero on 9/28/2016.
  */
 
 public class Game{
-    public static int players;
+
+
+    public static int player;
     public final int difficult;
     private String currentPlayer;
 
-    public Game(int players, int difficult){
-        this.players = 1;
+    public Game(int player, int difficult){
+        this.player = player;
         this.difficult = difficult;
         currentPlayer = "x";
     }
+
+    public int getRondomTile(){
+        Random randomTile = new Random();
+        return randomTile.nextInt(9);
+    }
+    public int getPlayer() {  return player; }
 
     public String getCurrentPlayer() {
         return currentPlayer;
--- app/src/main/java/com/example/elleu/tictactoe_android/MainActivity.java
+++ app/src/main/java/com/example/elleu/tictactoe_android/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.elleu.tictactoe_android;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -12,51 +12,112 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
-    private Button btnOnePlayer, btnTwoPlayer;
-    private int [] TILE =new int[9];
+    private int player;
+    private int difficult;
+    Button onePlayerButton,twoPlayerButton;
+    private int[] TILES;
     private Game game;
-    private int player = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnOnePlayer = (Button) findViewById(R.id.onePlayer);
-        btnTwoPlayer = (Button) findViewById(R.id.twoPlayer);
+        //Initializing each tile and saving in the array
+        TILES = new int[9];
 
-        //Initialize each tile
-        TILE[0] = R.id.a1;
-        TILE[1] = R.id.a2;
-        TILE[2] = R.id.a3;
-        TILE[3] = R.id.b1;
-        TILE[4] = R.id.b2;
-        TILE[5] = R.id.b3;
-        TILE[6] = R.id.c1;
-        TILE[7] = R.id.c2;
-        TILE[8] = R.id.c3;
+        TILES[0] = R.id.a1;
+        TILES[1] =R.id.a2;
+        TILES[2] = R.id.a3;
 
-        btnOnePlayer.setOnClickListener(this);
-        btnTwoPlayer.setOnClickListener(this);
+        TILES[3] = R.id.b1;
+        TILES[4] = R.id.b2;
+        TILES[5] = R.id.b3;
+
+        TILES[6] = R.id.c1;
+        TILES[7] = R.id.c2;
+        TILES[8] = R.id.c3;
+
+
+
+        onePlayerButton = (Button)findViewById(R.id.onePlayer);
+        onePlayerButton.setOnClickListener(this);
+        twoPlayerButton = (Button)findViewById(R.id.twoPlayer);
+        twoPlayerButton.setOnClickListener(this);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
-    public void onClick(View view){
+
+    public void onClick(View view){ //Thus method is called went click 1 player or two player nutton
         int id = view.getId();
-        if(id == R.id.onePlayer){
-            player = 1;
-            start();
-        }
-        else if(id == R.id.twoPlayer){
+        player = 1;
+        difficult = 0;
+
+        if(id == R.id.twoPlayer){
             player = 2;
-            start();
+        }
+
+        RadioGroup configDifficult = (RadioGroup)findViewById(R.id.radioGroup);
+        int idRadio = configDifficult.getCheckedRadioButtonId(); //Get radio button checked id
+        if(idRadio == R.id.normalDifificult){
+            difficult = 1;
+        }
+        else if(idRadio == R.id.hardDifificult)
+        {
+            difficult = 2;
+        }
+
+        game = new Game(difficult, player);
+
+        twoPlayerButton.setEnabled(false);
+        onePlayerButton.setEnabled(false);
+        configDifficult.setAlpha(0);
+
+        start();
+    }
+
+    private void start(){
+        ImageView image;
+        for(int tile : TILES){
+            image=(ImageView)findViewById(tile);
+            image.setImageResource(R.drawable.casilla);
         }
     }
 
     public void marcTile(View view){//This method is called went click in any tile in the board
 
-        if(game != null) { //Is the game does not start
-            for (int x = 0; x < TILE.length; x++) {
-                if (view.getId() == TILE[x]) {
-                    ImageView imageView = (ImageView)findViewById(TILE[x]);
+        if(game != null) { //Is the game exists does not start
+            int tile = 0;
+            for (int x = 0; x < 9; x++) {
+                if (TILES[x] == view.getId()) {
+                    tile = x;
+                    break;
+                }
+            }
+            marc(tile);
+            if(player == 1){ //if they is a player agains the machine get a random tile
+                tile = game.getRondomTile(); //After
+                marc(tile);
+            }
+        }
+    }
+
+    private void marc(int tile){
+        ImageView image = (ImageView)findViewById(TILES[tile]);
+
+        if(game.getCurrentPlayer() == "x"){
+            image.setImageResource(R.drawable.circulo);
+            game.setCurrentPlayer("y");
+        }else{
+            image.setImageResource(R.drawable.aspa);
+            game.setCurrentPlayer("x");
+        }
+    }
+
+
+         /*   for (int x = 0; x < TILES.length; x++) {
+                if (view.getId() == TILES[x]) {
+                    ImageView imageView = (ImageView)findViewById(TILES[x]);
                     if(game.getCurrentPlayer() == "x"){
                         imageView.setImageResource(R.drawable.circulo);
                         game.setCurrentPlayer("y");
@@ -67,10 +128,12 @@ public class MainActivity extends Activity implements View.OnClickListener{
                     break;
                 }
             }
-        }
+            */
+
+/*
+    private void checkMarkTile(){
+
     }
-
-
     private void start(){ //Thus methos should be called went click 1 player or two player
         ImageView image;
         for(int tiles : TILE){ //Reset the board
@@ -95,4 +158,5 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
         game = new Game(player, selectedDifficult); //Start the game!
     }
+    */
 }
